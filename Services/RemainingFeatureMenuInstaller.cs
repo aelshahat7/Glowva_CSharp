287edc3178cd3d60144fbb4afa4212fe1c510839
aelshahat7 <a.elshahat7@gmail.com>
2026-08-31 19:06:52 +0300

    Open advanced feature menus as MDI children

--- Services/RemainingFeatureMenuInstaller.cs
+++ Services/RemainingFeatureMenuInstaller.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Windows.Forms;
 using GlowvaERP.Forms;
 
 namespace GlowvaERP.Services;
@@ -6,14 +8,32 @@ public static class RemainingFeatureMenuInstaller
 {
     public static void Install(Form shell)
     {
-        var menu=shell.MainMenuStrip;
-        if(menu==null)return;
-        var root=new ToolStripMenuItem("الأدوات المتقدمة"){RightToLeft=RightToLeft.Yes};
-        Add(root,"تسوية وجرد المخزون",()=>Show(shell,new InventoryAdjustmentForm()));
-        Add(root,"المستخدمون والصلاحيات",()=>Show(shell,new UserManagementForm()));
-        Add(root,"مركز التقارير",()=>Show(shell,new ReportCenterForm()));
+        var menu = shell.MainMenuStrip;
+        if (menu == null) return;
+
+        var root = new ToolStripMenuItem("الأدوات المتقدمة") { RightToLeft = RightToLeft.Yes };
+        Add(root, "تسوية وجرد المخزون", shell, () => new InventoryAdjustmentForm());
+        Add(root, "المستخدمون والصلاحيات", shell, () => new UserManagementForm());
+        Add(root, "مركز التقارير", shell, () => new ReportCenterForm());
         menu.Items.Add(root);
     }
-    private static void Add(ToolStripMenuItem parent,string text,Action action){var x=new ToolStripMenuItem(text){RightToLeft=RightToLeft.Yes};x.Click+=(_,_)=>action();parent.DropDownItems.Add(x);}
-    private static void Show(Form owner,Form form){using(form)form.ShowDialog(owner);}
+
+    private static void Add(ToolStripMenuItem parent, string text, Form shell, Func<Form> factory)
+    {
+        var x = new ToolStripMenuItem(text) { RightToLeft = RightToLeft.Yes };
+        x.Click += (_, _) => Open(shell, factory, text);
+        parent.DropDownItems.Add(x);
+    }
+
+    private static void Open(Form shell, Func<Form> factory, string title)
+    {
+        if (shell is WorkspaceShellForm workspace)
+        {
+            workspace.OpenChild(factory, title);
+            return;
+        }
+
+        using var form = factory();
+        form.ShowDialog(shell);
+    }
 }
